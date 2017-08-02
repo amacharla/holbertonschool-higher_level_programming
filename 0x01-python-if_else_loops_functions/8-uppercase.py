75916a133d363bde0f6e54050ed20e440546046e
amacharla <noop2016@gmail.com>
2017-08-02 01:04:47 +0000

    7-12 done

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-
+def uppercase(str):
+    for i in str[:]:
+        char = ord(i)
+        if char >= 97 and char <= 122:
+            print("{:s}".format(chr(char - 32)), end="")
+        else:
+            print(i, end="")
+    print()
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
-
+def print_last_digit(number):
+    num = str(number)
+    lastdigit = int(num[-1])
+    print(lastdigit, end="")
+    return lastdigit
