17f599851d08c6a8329fd9ceef4a4c147ac0eb6e
amacharla <noop2016@gmail.com>
2017-08-03 22:20:01 +0000

    3 done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     len = len(argv) - 1
     print(len, "argument{}".format("." if len == 0 else "s:"))
     for index, value in enumerate(argv[1:], 1):
-        print(index, ":", value)
+        print("{:d}:".format(index), value)
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
-
+if __name__ == "__main__":
+    from sys import argv
+    result = 0
+    for num in argv[1:]:
+        result += int(num)
+    print(result)
