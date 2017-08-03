58685e001885b08e8022e87f64a7b2afa6b23b99
amacharla <noop2016@gmail.com>
2017-08-03 22:23:46 +0000

    2 singular argument fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     from sys import argv
     len = len(argv) - 1
-    print(len, "argument{}".format("." if len == 0 else "s:"))
+    print("{:d} argument{}".format(len, "." if len <= 1 else "s:"))
     for index, value in enumerate(argv[1:], 1):
         print("{:d}:".format(index), value)
