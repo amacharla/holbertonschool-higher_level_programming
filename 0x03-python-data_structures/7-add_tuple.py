7b1550e4e87f1ab9567ff07ab54c739a75b2dc38
amacharla <noop2016@gmail.com>
2017-08-09 06:45:35 +0000

    to 10 done

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     if tuple_a and tuple_b:
+        result = []
         for i in range(2):
-            result =
+            a1 = tuple_a[i] if len(tuple_b) == 2 else 0
+            a2 = tuple_b[i] if len(tuple_b) == 2 else 0
+            result.append(a1 + a2)
+        return (tuple(result))
+    return
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-
+    if my_list:
+        new_list = my_list.copy()
+        new_list.sort()
+        return (new_list[-1])
+    return (None)
