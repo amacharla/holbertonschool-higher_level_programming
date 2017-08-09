96e6775f7b6dcf941267edea64460ae334127e5d
amacharla <noop2016@gmail.com>
2017-08-09 02:27:26 +0000

    2-4 done issue with 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-
+    for index, char in enumerate(my_string[:]):
+        if char == 'c' or char == 'C':
+            new_string = my_string[:index] + my_string[index + 1:]
+    return (new_string)
