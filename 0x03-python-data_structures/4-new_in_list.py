e895299799a55f96fdfb7036ec1ea8715b129ce7
amacharla <noop2016@gmail.com>
2017-08-09 07:12:27 +0000

    4 arg checks

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if my_list:
+    if my_list and idx >= 0 or idx < len(my_list) - 1:
         new_list = my_list.copy()
         if idx <= len(my_list) - 1:
             new_list[idx] = element
             return (new_list)
-        return (new_list)
+    return (my_list)
