fef3a847306f1b5bbce0ee897fcb54a83ca01d7b
amacharla <noop2016@gmail.com>
2017-08-09 06:47:13 +0000

    10 retry

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     if my_list:
-        new_list = my_list.copy()
+        new_list = my_list
         for i in new_list:
             new_list[i] = True if new_list[i] % 2 == 0 else False
         return (new_list)
