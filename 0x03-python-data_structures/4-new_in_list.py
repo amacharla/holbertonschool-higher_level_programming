cba5d3137359f952a68ac0f86983444c4a165660
amacharla <noop2016@gmail.com>
2017-08-09 06:00:01 +0000

    4 added argument checks

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if my_list:
+    if my_list and idx and element:
         new_list = my_list.copy()
         if idx <= len(my_list) - 1:
             new_list[idx] = element
