2ca5094ec0c55c23930924b4c28efffcc394cb4a
amacharla <noop2016@gmail.com>
2017-08-11 02:07:03 +0000

    11 renamed and 1 r1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list and search and replace:
+    if my_list:
         return (list(map(lambda num: replace if num == search else num, my_list)))
     return
