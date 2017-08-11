ac5a2fb410a177a7e96ae59a3e5bff5f121f8c31
amacharla <noop2016@gmail.com>
2017-08-11 21:51:07 +0000

    1 fixed

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list and len(my_list) != 0:
-        return (list(map(lambda num: replace if num == search else num, my_list)))
-    return
+    return (list(map(lambda num: replace if num == search else num, my_list)))
