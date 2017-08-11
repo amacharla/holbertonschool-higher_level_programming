dd7521f4cfe5d490331da9f4553db607ec377e0b
amacharla <noop2016@gmail.com>
2017-08-11 21:56:36 +0000

    3 and 4 done

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    if set_1 and set_2:
-        result = []
-        for a in set_1:
-            for b in set_2:
-                if a == b:
-                    result.append(a)
-        return result
-    return
+    return (set_1 & set_2)
