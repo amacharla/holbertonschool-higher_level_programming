d59ac608e4c8c66fb0b59c53ef28a400ba68a3bb
amacharla <noop2016@gmail.com>
2017-08-23 20:48:35 +0000

    4 and 5 reconfigured __init__ to call setter func

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -2,18 +2,11 @@
 class Square:
     def __init__(self, size=0):
         """
+        Calls size.setter func
         Args:
             size: must be int and greater than 0
-        Raises:
-            TypeError: if size is not int
-            ValueError: size is less than 0
         """
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.size = size
 
     def area(self):
         """ Returns: current square area """
