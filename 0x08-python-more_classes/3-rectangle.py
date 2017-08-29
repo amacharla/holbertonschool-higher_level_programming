fcb4301555559579585bec8261f2d142ef794f39
amacharla <noop2016@gmail.com>
2017-08-29 00:22:05 +0000

    1-6 done

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+class Rectangle:
+    """ Sets width and height of Rectanle """
+
+    def __init__(self, width=0, height=0):
+        """ Initilizes and calls respective setter method
+        Args:
+            width: int > 0
+            height: int > 0
+        """
+        self.width, self.height = width, height
+
+    @property
+    def width(self):
+        """ width: has to be integer and greater than 0 """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ height: has to be integer and greater than 0 """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """ Returns: area of Rectangle """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """ Returns: parimeter of Rectangle else 0 if height or width == 0 """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """ String representation of Rectangle class.
+        Returns: str rectangle in '#' form else empty string.
+        """
+        __string = ""
+
+        if self.__width == 0 or self.__height == 0:
+            return __string
+
+        for h in range(self.__height):
+            for w in range(self.__width):
+                __string += '#'
+            __string += '\n'
+        return __string[:-1] #  avoid sending last newline character
