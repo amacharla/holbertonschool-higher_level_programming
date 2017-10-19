abbcf7b7963194da1c61cd2f8d3170bdac78bbdd
Anoop M <noop2016@gmail.com>
2017-10-19 05:41:50 +0000

    fixed all mistakes

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,7 +3,7 @@
 -- Don’t display a genre that doesn’t have any shows linked
 -- Results must be sorted in descending order by the number of shows linked
 
-SELECT tv_genres.name, COUNT(tv_show_genres.show_id) as "number_shows"
+SELECT tv_genres.name AS "genre", COUNT(tv_show_genres.show_id) AS "number_shows"
 	FROM tv_genres, tv_show_genres
 	WHERE tv_genres.id = tv_show_genres.genre_id
 	GROUP BY tv_show_genres.genre_id
--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,7 +2,7 @@
 -- add Foreign key for state_id <- state (id)
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
 	(id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
 	state_id INT NOT NULL, FOREIGN KEY (state_id) REFERENCES states(id), 
        	name VARCHAR(256) NOT NULL);
