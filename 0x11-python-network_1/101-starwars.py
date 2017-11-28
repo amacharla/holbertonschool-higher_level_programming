0029162e2e527eba1abf8189f9406588061c4660
Anoop M <noop2016@gmail.com>
2017-11-28 06:53:38 +0000

    101 done

--- 0x11-python-network_1/101-starwars.py
+++ 0x11-python-network_1/101-starwars.py
@@ -6,6 +6,7 @@ convert JSON -> dict
 print count then iter through list of dicts of results
 print name
 https://swapi.co/api/people/?search=r2
+AUTOMATICALLY GETS TO NEXT PAGE OF RESULTS
 """
 
 if __name__ == "__main__":
@@ -16,5 +17,13 @@ if __name__ == "__main__":
                        params={'search': argv[1]})
     rjson = res.json()
     print("Number of results:", rjson.get('count'))
+
+    while rjson.get('next'):
+        nextp = rjson.get('next')
+        for character in rjson.get('results'):
+            print(character.get('name'))
+        res = requests.get(nextp)
+        rjson = res.json()
+
     for character in rjson.get('results'):
         print(character.get('name'))
