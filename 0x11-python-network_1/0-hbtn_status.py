e1bd00b012920a47d2250e9eef7e3e399e3be45b
Anoop M <noop2016@gmail.com>
2017-11-28 01:47:14 +0000

    added header gaurd for 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """ Display the attributes of response object """
 
-from urllib.request import urlopen
+if __name__ == "__main__":
+    from urllib.request import urlopen
 
-with urlopen('https://intranet.hbtn.io/status') as response:
-    page = response.read()
+    with urlopen('https://intranet.hbtn.io/status') as response:
+        page = response.read()
 
-print("Body response:"
-      "\n\t- type: ", type(page),
-      "\n\t- content: ", page,
-      "\n\t- utf8 content: ", page.decode("utf-8"),
-      "\n", sep="", end="")
+    print("Body response:"
+          "\n\t- type: ", type(page),
+          "\n\t- content: ", page,
+          "\n\t- utf8 content: ", page.decode("utf-8"),
+          "\n", sep="", end="")
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,6 +4,7 @@ Takes URL
 sends request
 displays response header for 'X-Request-Id'
 """
+
 if __name__ == "__main__":
     from sys import argv
     from urllib.request import urlopen
