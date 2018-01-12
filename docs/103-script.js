74bbc0eb253305eca343d92555b157e1dc5bae96
Anoop M <noop2016@gmail.com>
2018-01-12 05:29:02 +0000

    testing time

---
+++ docs/103-script.js
@@ -0,0 +1,11 @@
+const url = 'https://query.yahooapis.com/v1/public/yql?';
+const data = {
+  q: 'select wind from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + $('INPUT#city_search').val() + '")',
+  format: 'json'
+};
+
+$('INPUT#btn_search').submit(function () {
+  $.getJSON(url, data, function (data) {
+    $('DIV#wind_speed').text(data.query.results.channel.wind.speed);
+  });
+});
