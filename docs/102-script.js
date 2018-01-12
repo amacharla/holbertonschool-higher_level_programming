9c2e1fcee9a639d1864b24580bd170ca7b5dfd2c
Anoop M <noop2016@gmail.com>
2018-01-12 08:25:24 +0000

    all taks done

--- docs/102-script.js
+++ docs/102-script.js
@@ -1,11 +1,14 @@
-const url = 'https://query.yahooapis.com/v1/public/yql?';
-const data = {
-  q: 'select wind from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + $('INPUT#city_search').val() + '")',
-  format: 'json'
-};
+$(function () {
+  const url = 'https://query.yahooapis.com/v1/public/yql?';
+  const name = $('INPUT#city_search').val();
+  const data = {
+    q: 'select wind from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + name + '")',
+    format: 'json'
+  };
 
-$('INPUT#btn_search').click(function () {
-  $.getJSON(url, data, function (data) {
-    $('DIV#wind_speed').text(data.query.results.channel.wind.speed);
+  $('INPUT#btn_search').click(function () {
+    $.getJSON(url, data, function (data) {
+      $('DIV#wind_speed').text(data.query.results.channel.wind.speed);
+    });
   });
 });
