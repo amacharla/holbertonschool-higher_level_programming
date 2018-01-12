de7136a8ff18e1fb836bb6d04c2264761c6b1d85
Anoop M <noop2016@gmail.com>
2018-01-12 01:08:13 +0000

    3 refactored code

--- docs/3-script.js
+++ docs/3-script.js
@@ -1,6 +1,4 @@
 $('DIV#red_header').click(function () {
-  $('header').addClass('red');
-});
-$('DIV#red_header').click(function () {
   $(this).css('color', '#FF0000');
+  $('header').addClass('red');
 });
